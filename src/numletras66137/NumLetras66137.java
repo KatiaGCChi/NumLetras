f6c41202fda1dcef04f12bef7f1e6e8cf14c9e37
TRUJEQUE <TRUJEQUE@ANDREA>
2021-03-01 18:48:36 -0600

    De numeros a letras

--- src/numletras66137/NumLetras66137.java
+++ src/numletras66137/NumLetras66137.java
@@ -19,84 +19,84 @@ public class NumLetras66137 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
-        int multiplo;
-        int unidad;
+        int multitud;
+        int cantidad;
         
         System.out.println("Introduce un número del 0 al 99: ");
         int num = entrada.nextInt();
     
     
-        multiplo=num/10 ;
-        unidad=num%10 ;
-        String[] unidades = {"cero", "uno", "dos", "Tres", "Cuatro", "cinco", "seis", "siete", "ocho", "nueve"};
+        multitud=num/10 ;
+        cantidad=num%10 ;
+        String[] cantidades = {"cero", "uno", "dos", "Tres", "Cuatro", "cinco", "seis", "siete", "ocho", "nueve"};
         String[] distinto = {"Diez", "Once", "Doce", "Trece", "Catorce", "Quince", "Dieciseis", "Diecisiete", "Dieciocho", "Diecinueve"};
     
-        switch (multiplo){
+        switch (multitud){
             case 0:
-                System.out.println(unidades[unidad]);
+                System.out.println(cantidades[cantidad]);
                  break;
             case 1:
-                System.out.println(distinto[unidad]);
+                System.out.println(distinto[cantidad]);
                 break; 
             case 2:
-                if( unidad==0){
+                if( cantidad==0){
                 System.out.println("Veinte");
             } else {
-                System.out.println ("Veinti"+unidades[unidad]); 
+                System.out.println ("Veinti"+cantidades[cantidad]); 
             }
                 break;
             case 3: 
-                if( unidad==0){
+                if( cantidad==0){
                  System.out.println("Treinta");
             } else {
-                 System.out.println ("Treinta y "+unidades[unidad]);
+                 System.out.println ("Treinta y "+cantidades[cantidad]);
             }
                  break;
             case 4: 
-                if( unidad==0){
+                if( cantidad==0){
                 System.out.println("Cuarenta");
             } else {
-                System.out.println ("Cuarenta y "+unidades[unidad]);
+                System.out.println ("Cuarenta y "+cantidades[cantidad]);
             }
                 break;
             case 5: 
-                 if( unidad==0){
+                 if( cantidad==0){
                  System.out.println("Cincuenta");
             } else {
-            System.out.println ("Cincuenta y "+unidades[unidad]);
+            System.out.println ("Cincuenta y "+cantidades[cantidad]);
             }
             break;
         case 6: 
-            if( unidad==0){
+            if( cantidad==0){
                 System.out.println("Sesenta");
             } else {
-                System.out.println ("Sesenta y "+unidades[unidad]);
+                System.out.println ("Sesenta y "+cantidades[cantidad]);
             }
                 break;
         case 7: 
-            if( unidad==0){
+            if( cantidad==0){
                 System.out.println("Setenta");
             } else {
-            System.out.println ("Setenta y "+unidades[unidad]);
+            System.out.println ("Setenta y "+cantidades[cantidad]);
             }
             break;
         case 8: 
-            if( unidad==0){
+            if( cantidad==0){
                 System.out.println("Ochenta");
             } else {
-            System.out.println ("Ochenta y "+unidades[unidad]);
+            System.out.println ("Ochenta y "+cantidades[cantidad]);
             }
             break;
         case 9: 
-            if( unidad==0){
+            if( cantidad==0){
                 System.out.println("Noventa");
             } else {
-            System.out.println ("Noventa y "+unidades[unidad]);
+            System.out.println ("Noventa y "+cantidades[cantidad]);
             }
              break ;
             
         default:
-            System.out.println ("numero no disponible");
+            System.out.println ("No esta disponible");
             break;
         }
         
